849c4beb4795ab5a2e3ab7c6199dbd18f496b08e
limsungju <fullmoon4860@gmail.com>
2019-09-09 11:18:22 +0900

    jdbc - SelectTest

---
+++ src/main/java/kr/co/itcen/jdbc/SelectTest.java
@@ -0,0 +1,62 @@
+package kr.co.itcen.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SelectTest {
+
+	public static void main(String[] args) {
+		Connection connection = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			// 1. JDBC Driver 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			// 2. 연결하기
+			String url = "jdbc:mariadb://192.168.1.84:3306/webdb?characterEncoding=utf8";
+			// url, db계정, db비밀번호
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			// 3. Statment 객체 생성(받아오기)
+			stmt = connection.createStatement();
+			
+			// 4. SQL문 실행
+			String sql = "select no, name from department";
+			rs = stmt.executeQuery(sql);
+			
+			// 5. 결과 가져오기
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				
+				System.out.println(no + ":" + name);
+			}
+			
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Fail to Loading Driver:" + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(stmt != null) {
+					stmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
