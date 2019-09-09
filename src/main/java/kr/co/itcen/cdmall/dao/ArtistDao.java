6cf58c495c75181627e84d87794caa0c375e213a
limsungju <fullmoon4860@gmail.com>
2019-09-09 14:52:44 +0900

    jdbc - ArtistDao

---
+++ src/main/java/kr/co/itcen/cdmall/dao/ArtistDao.java
@@ -0,0 +1,109 @@
+package kr.co.itcen.cdmall.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.itcen.cdmall.vo.ArtistVo;
+
+
+public class ArtistDao {
+
+	public Boolean insert(ArtistVo vo) {
+		Boolean result = false;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			connection = getConnection();
+			
+			String sql = "insert into artist values(null, ?)";
+			pstmt = connection.prepareStatement(sql);
+			
+			pstmt.setString(1, vo.getName());
+			
+			int count = pstmt.executeUpdate();
+			
+			result = (count == 1); 
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public List<ArtistVo> getList() {
+		List<ArtistVo> result = new ArrayList<ArtistVo>();
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			connection = getConnection();
+			
+			String sql = "select no, name from artist order by no asc";
+			pstmt = connection.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				
+				ArtistVo vo = new ArtistVo();
+				vo.setNo(no);
+				vo.setName(name);
+				
+				result.add(vo);
+			}
+						
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection connection = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			String url = "jdbc:mariadb://192.168.1.84:3306/cdmall?characterEncoding=utf8";
+			connection = DriverManager.getConnection(url, "cdmall", "cdmall");
+		} catch(ClassNotFoundException e) {
+			System.out.println("Fail to Loading Driver:" + e);
+		} 
+		
+		return connection;
+	}
+
+}
