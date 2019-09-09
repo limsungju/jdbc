576d931ffdd8cf9511958ac691300b4388823534
limsungju <fullmoon4860@gmail.com>
2019-09-09 15:23:45 +0900

    jdbc - ArtistDao delete기능, 마지막 no번호 확인하는 기능 추가

--- src/main/java/kr/co/itcen/cdmall/dao/ArtistDao.java
+++ src/main/java/kr/co/itcen/cdmall/dao/ArtistDao.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,23 +18,38 @@ public class ArtistDao {
 		Boolean result = false;
 		Connection connection = null;
 		PreparedStatement pstmt = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 
 		try {
 			connection = getConnection();
 			
 			String sql = "insert into artist values(null, ?)";
 			pstmt = connection.prepareStatement(sql);
-			
 			pstmt.setString(1, vo.getName());
-			
 			int count = pstmt.executeUpdate();
 			
 			result = (count == 1); 
+			
+			stmt = connection.createStatement();
+			// mysql에만 있는 함수
+			rs = stmt.executeQuery("select last_insert_id()");
+			if(rs.next()) {
+				Long no = rs.getLong(1);
+				vo.setNo(no);
+			}
+			
 			
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
 			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(stmt != null) {
+					stmt.close();
+				}
 				if(pstmt != null) {
 					pstmt.close();
 				}
@@ -92,6 +108,34 @@ public class ArtistDao {
 		return result;
 	}
 	
+	public void delete() {
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			connection = getConnection();
+			
+			String sql = "delete from artist";
+			pstmt = connection.prepareStatement(sql);
+						
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	private Connection getConnection() throws SQLException {
 		Connection connection = null;
 		try {
@@ -104,5 +148,7 @@ public class ArtistDao {
 		} 
 		return connection;
 	}
+
+	
 
 }
