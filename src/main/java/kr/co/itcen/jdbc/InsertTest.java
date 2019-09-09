334fb3ed695c5e7f08d731c3633426e1d8e41092
limsungju <fullmoon4860@gmail.com>
2019-09-09 11:27:31 +0900

    jdbc - InsertTest

---
+++ src/main/java/kr/co/itcen/jdbc/InsertTest.java
@@ -0,0 +1,57 @@
+package kr.co.itcen.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertTest {
+	public static void main(String[] args) {
+		insert("경영지원팀1");
+		insert("경영지원팀2");
+		insert("경영지원팀3");
+	}
+	
+	public static boolean insert(String name) {
+		Boolean result = false;
+		
+		Connection connection = null;
+		Statement stmt = null;
+
+		try {
+			// 1. JDBC Driver 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			// 2. 연결하기
+			String url = "jdbc:mariadb://192.168.1.84:3306/webdb?characterEncoding=utf8";
+			// url, db계정, db비밀번호
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			// 3. Statment 객체 생성(받아오기)
+			stmt = connection.createStatement();
+			
+			// 4. SQL문 실행
+			String sql = "insert into department values(null, '" + name + "')";
+			int count = stmt.executeUpdate(sql);
+			
+			result = (count == 1); 
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Fail to Loading Driver:" + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(stmt != null) {
+					stmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+}
