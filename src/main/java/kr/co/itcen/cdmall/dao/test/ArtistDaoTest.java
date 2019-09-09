2b82ab977ffab881b8c6490f495f8116ee74c775
limsungju <fullmoon4860@gmail.com>
2019-09-09 15:23:11 +0900

    jdbc - ArtistDaoTest inset, select, deleteAll Test

--- src/main/java/kr/co/itcen/cdmall/dao/test/ArtistDaoTest.java
+++ src/main/java/kr/co/itcen/cdmall/dao/test/ArtistDaoTest.java
@@ -9,28 +9,36 @@ public class ArtistDaoTest {
 
 	public static void main(String[] args) {
 		insertTest();
-		
 		selectTest();
-		
+		deleteAllTest();
+	}
+	
+	private static void deleteAllTest() {
+		new ArtistDao().delete();
 	}
 	
 	private static void insertTest() {
+		System.out.println("-----insert test-----");
 		ArtistDao dao = new ArtistDao();
 		
 		ArtistVo vo1 = new ArtistVo();
 		vo1.setName("아이유");
 		dao.insert(vo1);
+		System.out.println(vo1);
 		
 		ArtistVo vo2 = new ArtistVo();
 		vo2.setName("선미");
 		dao.insert(vo2);
+		System.out.println(vo2);
 		
 		ArtistVo vo3 = new ArtistVo();
 		vo3.setName("신비");
 		dao.insert(vo3);
+		System.out.println(vo3);
 	}
 	
 	private static void selectTest() {
+		System.out.println("-----select test-----");
 		ArtistDao dao = new ArtistDao();
 		List<ArtistVo> list = dao.getList();
 		for(ArtistVo vo:list) {
